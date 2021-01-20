4751c5ed084430fcafbfbaed87d95888182ae0b4
tungjj <tungplatin@gmail.com>
2021-01-20 22:35:32 +0700

    done nodemon

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-var x;
-app.get('/', (req, res) => res.send('Hello !'));
-app.listen(port, () => console.log(`Example app listening on port port!`));
