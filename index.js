9f8f19bfcfa339864d8c11b61505ad155cfb6682
tungjj <tungplatin@gmail.com>
2021-02-01 16:48:18 +0700

    error-- cannot post method

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({  extended: true}));
+
 app.set('view engine', 'pug')
 app.set('views', './views')
 
 app.get('/', (req, res) => res.render('addAcc')   );//!-->>xem lai cach truyen tham so trong ham render
 app.get('/manage', (req, res) => res.render('manage'));
+
+app.post('/manage', (req, res) =>{
+    // res.render('manage', {name:req.name});
+    console.log(req.body);
+    console.log(req.body.email);
+});
 
 app.listen(port, () => console.log(`Example app listening on port port!`));
